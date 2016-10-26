04bd7cb5788957e1e04b1211c18c7f9a87b77956
Kevin Schiroo <kjschiroo@gmail.com>
2016-10-25 21:37:17 -0500

    Adding non-react path

--- app/controllers.py
+++ app/controllers.py
@@ -1,5 +1,7 @@
 from app import app, models, db
-from flask import request, jsonify, render_template
+from flask import request, jsonify, render_template, url_for
+from urllib.parse import urlencode
+
 
 
 Salary = models.Salary
@@ -25,10 +27,14 @@ def _construct_salary_query(args):
 
 @app.route("/api/salaries")
 def get_salaries():
-    def_limit = 100
-    limit = min(int(request.args.get("limit", def_limit)), def_limit)
-    cont = int(request.args.get("batch_continue", 0))
-    query = _construct_salary_query(request.args)
+    return jsonify(_get_salaries(request.args))
+
+
+def _get_salaries(query_params):
+    def_limit = 50
+    limit = min(int(query_params.get("limit", def_limit)), def_limit)
+    cont = int(query_params.get("batch_continue", 0))
+    query = _construct_salary_query(query_params)
     query = query.filter(Salary.id > cont)
     result = [s for s in query.order_by(Salary.id).limit(limit)]
     to_return = {
@@ -37,7 +43,7 @@ def get_salaries():
     if len(result) > 0:
         max_id = result[-1].id
         to_return["batch_continue"] = max_id + 1
-    return jsonify(to_return)
+    return to_return
 
 
 @app.route("/api/add_salary", methods=["post"])
@@ -64,3 +70,16 @@ def add_salary():
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/noreact")
+def no_react():
+    args = {}
+    for key in request.args:
+        args[key] = request.args.get(key)
+    salary_data = _get_salaries(args)
+    args["batch_continue"] = salary_data["batch_continue"]
+    next_url = url_for("no_react", **args)
+    return render_template("no_react.html",
+                           data=salary_data["data"],
+                           next_url=next_url)
