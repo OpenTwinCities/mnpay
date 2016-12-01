0fcacd7f0022fe61506a4d13c5170b0e1920cff5
Kevin Schiroo <kjschiroo@gmail.com>
2016-11-30 21:55:43 -0600

    simplify control event handling for #14

--- client/js/components/Query/Control.js
+++ client/js/components/Query/Control.js
@@ -13,32 +13,8 @@ export default class Control extends React.Component {
                 }
   }
 
-  on_first_name_change(e){
-    this.setState({first_name: e.target.value})
-  }
-
-  on_last_name_change(e){
-    this.setState({last_name: e.target.value})
-  }
-
-  on_agency_change(e){
-    this.setState({agency: e.target.value})
-  }
-
-  on_dept_change(e){
-    this.setState({dept: e.target.value})
-  }
-
-  on_title_change(e){
-    this.setState({title: e.target.value})
-  }
-
-  on_sortby_change(e){
-    this.setState({sortby: e.target.value})
-  }
-
-  on_direction_change(e){
-    this.setState({direction: e.target.value})
+  on_change(e){
+    this.setState({[e.target.id]: e.target.value})
   }
 
   componentDidMount () {
@@ -82,29 +58,29 @@ export default class Control extends React.Component {
         <div className="form-group">
           <div className="col-sm-4">
             <label for="first_name">First name:</label>
-            <input type="text" className="form-control" id="first_name" onChange={ this.on_first_name_change.bind(this) } name="first_name" value={ this.state.first_name }/>
+            <input type="text" className="form-control" id="first_name" onChange={ this.on_change.bind(this) } name="first_name" value={ this.state.first_name }/>
           </div>
           <div className="col-sm-4">
             <label for="last_name">Last name:</label>
-            <input type="text" className="form-control" id="last_name" name="last_name" onChange={ this.on_last_name_change.bind(this)} value={ this.state.last_name }/>
+            <input type="text" className="form-control" id="last_name" name="last_name" onChange={ this.on_change.bind(this)} value={ this.state.last_name }/>
           </div>
           <div className="col-sm-4">
             <label for="title">Title:</label>
-            <input type="text" className="form-control" id="title" name="title" onChange={ this.on_title_change.bind(this)} value={ this.state.title }/>
+            <input type="text" className="form-control" id="title" name="title" onChange={ this.on_change.bind(this)} value={ this.state.title }/>
           </div>
         </div>
         <div className="form-group">
           <div className="col-sm-4">
             <label for="agency">Agency:</label>
-            <input type="text" className="form-control" id="agency" name="agency" onChange={ this.on_agency_change.bind(this)} value={ this.state.agency }/>
+            <input type="text" className="form-control" id="agency" name="agency" onChange={ this.on_change.bind(this)} value={ this.state.agency }/>
           </div>
           <div className="col-sm-4">
             <label for="dept">Dept:</label>
-            <input type="text" className="form-control" id="dept" name="dept" onChange={ this.on_dept_change.bind(this)} value={ this.state.dept }/>
+            <input type="text" className="form-control" id="dept" name="dept" onChange={ this.on_change.bind(this)} value={ this.state.dept }/>
           </div>
           <div className="col-sm-2">
-            <label for="sort_field">Order by:</label>
-            <select className="form-control" id="sort_field" value={this.state.sortby} onChange={this.on_sortby_change.bind(this)}>
+            <label for="sortby">Order by:</label>
+            <select className="form-control" id="sortby" value={this.state.sortby} onChange={this.on_change.bind(this)}>
               <option value="first_name">First name</option>
               <option value="last_name">Last name</option>
               <option value="agency">Agency</option>
@@ -116,7 +92,7 @@ export default class Control extends React.Component {
           </div>
           <div className="col-sm-2">
             <label for="direction">Direction:</label>
-            <select className="form-control" id="direction" value={this.state.direction} onChange={this.on_direction_change.bind(this)}>
+            <select className="form-control" id="direction" value={this.state.direction} onChange={this.on_change.bind(this)}>
               <option value="asc">Ascending</option>
               <option value="desc">Descending</option>
             </select>
